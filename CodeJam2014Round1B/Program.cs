2f3e22ba47541358348cd788e5aa7b77b808ff0d
SamTheDev <abdelkhalekdev@Gmail.Com>
2014-05-03 13:29:22 -0400

    First Commit

---
+++ CodeJam2014Round1B/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleSourceControlProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Console.WriteLine("First Commit");
+            Console.ReadLine();
+        }
+    }
+}
