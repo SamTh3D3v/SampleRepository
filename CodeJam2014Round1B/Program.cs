86410362c74ffd544a44aa99deb92db33e57fb65
SamTheDev <abdelkhalekdev@Gmail.Com>
2014-05-03 14:28:50 -0400

    First Commit From CptPlusPlus

--- CodeJam2014Round1B/Program.cs
+++ CodeJam2014Round1B/Program.cs
@@ -11,7 +11,7 @@ namespace SampleSourceControlProject
     {
         static void Main(string[] args)
         {
-           Console.WriteLine("Third Commit");
+           Console.WriteLine("Third Commit From CptPlusPlus");
             Console.ReadLine();
         }
     }
